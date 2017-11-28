734b3eb0ef12b274df4e096bd3cf83da5f58c43e
sangam92 <sangam92@users.noreply.github.com>
2017-11-28 22:27:27 +0530

    Add files via upload

--- pygoogleapi.py
+++ pygoogleapi.py
@@ -9,27 +9,43 @@ import enchant
 #from enchant.tokenize import get_tokenizer
 #top=tkinter.Tk()
 d=enchant.Dict("en_US")
+f=enchant.Dict("fr_FR")
 import speech_recognition as sr
+#socgen_word= {XONE: }
 #tknzr = get_tokenizer("en_US")
 #spell = []
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Hi This is Kiku!!Say Something!!!")
     audio = r.listen(source)
-#    spell = audio
+   
     
- #   for w in tknzr(spell):
-  #      d.check(spell)
+    
 
-#    d.check(audio)
 
 try:
    
     print("You said: " + r.recognize_google(audio))
 except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
+     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
 
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
     
 #    top.mainloop()
+    
+b= r.recognize_google(audio)
+
+print(b)
+
+for word in b.split():
+    a=d.check(word)
+    #print(word)
+    #print(a)
+    
+    if (a == True):
+        print('english')
+    else:
+        for word in b.split():
+            a=f.check(word)
+        print('French')
